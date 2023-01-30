4e039b764644b9995554c694df635fc516cc592f
fayaz-rafin <fayaz.rafin@gmail.com>
2023-01-30 12:56:03 -0500

    made separate folders for our frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './navbar';
+import Navbar from './navbar';
+
+
+function App() {
+  return (
+        <div>
+          <Navbar />
+        </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
