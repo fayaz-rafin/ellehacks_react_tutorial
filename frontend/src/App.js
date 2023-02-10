e69ca5b68640d3d5e32601b0019104e24944357a
fayaz-rafin <fayaz.rafin@gmail.com>
2023-02-10 17:24:11 -0500

    added a new news card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,50 @@
 import './App.css';
 import './components/Navbar';
 import Navbar from './components/Navbar';
+import NewsCards from './components/NewsCards'
+import './NewCards.css';
+import { Stack, Grid, Box } from '@mui/material';
+
 
 
 function App() {
   return (
-        <div>
-          <Navbar />
-        </div>
+        <><div>
+      <Box sx={{ display: 'flex' }}>
+      <Navbar />
+      </Box>
+    </div>
+    <div>
+      
+    </div>
+    <div>
+      <h1>
+        Toronto Headlines
+      </h1>
+        <Box sx={{ display: 'inline-flex', margin: 2, padding: 2 }}>
+          <NewsCards />
+        </Box>
+        <Box sx={{ display: 'inline-flex', margin: 2, padding: 2 }}>
+          <NewsCards />
+        </Box>
+        <Box sx={{ display: 'inline-flex', margin: 2, padding: 2 }}>
+          <NewsCards />
+        </Box>
+        <h1>
+          Across Ontario
+        </h1>
+        <Box sx={{ display: 'inline-flex', margin: 2, padding: 2}}>
+          <NewsCards />
+        </Box>
+        <Box sx={{ display: 'inline-flex', margin: 2, padding: 2 }}>
+          <NewsCards />
+        </Box>
+        <Box sx={{ display: 'inline-flex', margin: 2, padding: 2 }}>
+          <NewsCards />
+        </Box>
+
+      </div></>
+        
   );
 }
 
